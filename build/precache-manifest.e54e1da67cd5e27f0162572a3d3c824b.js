0c608b673484673c26d5441e85d689167e917c28
Reggie Alleyne <reggiealleyne89@gmail.com>
2020-04-08 19:42:33 -0700

    fix readme

---
+++ build/precache-manifest.e54e1da67cd5e27f0162572a3d3c824b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd07893a73159376f279fda0edd17ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df878c9d4c245e88f860",
+    "url": "/static/css/main.1813d8a7.chunk.css"
+  },
+  {
+    "revision": "200d632238ffb2304c4e",
+    "url": "/static/js/2.4baf66d5.chunk.js"
+  },
+  {
+    "revision": "df878c9d4c245e88f860",
+    "url": "/static/js/main.10e13f76.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/static/media/bg.2810fe6b.png"
+  }
+]);
